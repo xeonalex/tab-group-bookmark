f5b603210bd880794336b0fa2ebd3dadc62322c3
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2023-02-24 00:25:14 +0200

    [ default-popup.tsx ]: added displaying of existing tabs groups

--- src/pages/default-popup.tsx
+++ src/pages/default-popup.tsx
@@ -1,17 +1,38 @@
 import React, {useEffect, useMemo, useState} from "react";
+import styles from '@/styles/pages/DefaultPopup.module.scss'
+import classNames from "classnames";
+import {Spin, Tag} from "antd";
 
 export default function DefaultPopup() {
     const [tabGroups, setTabGroups] = useState<chrome.tabGroups.TabGroup[]>([])
+    const [loader, setLoader] = useState<boolean>(false)
+    const [error, setError] = useState<boolean>(false)
 
     useEffect(() => {
         (async () => {
-            setTabGroups(await chrome.tabGroups.query({}))
+            try {
+                setLoader(true)
+                setTabGroups(await chrome.tabGroups.query({}))
+                setLoader(false)
+            } catch (e) {
+                setError(true)
+            }
         })()
     }, [])
 
     return (
-        <>
-            {tabGroups.map((t) => t.title)}
-        </>
+        <div className={classNames(styles.container, 'p-m')}>
+            {loader ? (
+                <Spin spinning/>
+            ) : (
+                <>
+                    <div>Tab groups: </div>
+                    {error && 'Error'}
+                    {tabGroups.length === 0 && 'empty'}
+                    {tabGroups.map((t) => <Tag color={t.color} key={t.id}>{t.title}</Tag>)}
+                </>
+            )}
+
+        </div>
     )
 }
